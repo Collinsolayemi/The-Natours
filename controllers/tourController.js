4ff5adc36b720d281bbb93c0541f709af42f4267
collins <collinsolayemi@gmail.com>
2023-01-09 18:00:06 -0800

    changes

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -64,6 +64,12 @@ exports.getAllTours = async (req, res) => {
 
     query = query.skip(skip).limit(limit);
 
+    if (req.query.page) {
+      const numTours = await Tour.countDocuments()
+      if (skip >= numTours) throw new Error('This page does not exist')
+
+    }
+
     // EXECUTE THE QUERY
     const tours = await query;
 
