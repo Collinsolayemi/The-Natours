5deeec0a8489d943e114b53eb88a50d7dbe44b89
collins <collinsolayemi@gmail.com>
2023-01-16 06:02:15 -0800

    updating constructor functcion

--- utilities/apiFeatures.js
+++ utilities/apiFeatures.js
@@ -8,11 +8,9 @@ class APIfeature {
     const excludedFields = ['page', 'sort', 'limit', 'fields'];
     excludedFields.forEach((el) => delete queryObj[el]);
 
-    //1b. Advanced filtering
     let queryStr = JSON.stringify(queryObj);
     queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
 
-    // let query = Tour.find(JSON.parse(queryStr));
     this.query = this.query.find(JSON.parse(queryStr));
 
     return this;
