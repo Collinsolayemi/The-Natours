f05579c057f92ba4b8525663e40e880ea2c46ba6
collins <collinsolayemi@gmail.com>
2023-01-23 15:30:55 -0800

    added more functionality

--- server.js
+++ server.js
@@ -21,55 +21,9 @@ mongoose
   })
   .then(() => console.log('DB connected'));
 
-// const tourSchema = new mongoose.Schema({
-//   name: {
-//     type: String,
-//     required: [true, 'A tour must have a name'],
-//     unique: true,
-//   },
-//   rating: {
-//     type: Number,
-//     default: 4.5,
-//   },
-//   price: {
-//     type: Number,
-//     required: [true, 'A tour must have a price'],
-//   },
-// });
-
-// //creating a model
-// const Tour = mongoose.model('Tour', tourSchema);
-
 const testTour = new Tour({
   name: 'The Sailing Ship',
   price: 345,
-});
-
-// testTour
-//   .save()
-//   .then((doc) => {
-//     console.log(doc);
-//   })
-//   .catch((err) => {
-//     console.log('ERROR', err);
-//   });
-
-// JUST TO TEST THE TOUR CREATED TO THE DATABASE
-// const testTour = new Tour({
-//   name: 'The Lion Kingdom',
-//   rating: 4.9,
-//   price: 450,
-// });
-
-// testTour
-//   .save()
-//   .then((doc) => {
-//     console.log(doc);
-//   })
-//   .catch((err) => console.log('Error', err));
-
-app.get('/', (req, res) => {
-  res.send('servere');
 });
 
 const port = process.env.PORT || 3000;
