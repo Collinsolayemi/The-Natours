1f50a9a65d31cfe0d9006901f446e6b135e9293c
collinsolayemi <collinsolayemi@gmail.com>
2023-02-11 15:45:06 -0800

    updating the tour routes

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -2,12 +2,11 @@ const express = require('express');
 const tourController = require('./../controllers/tourController');
 const router = express.Router();
 
+//route implementation and http request
 router.route('/tour-stats').get(tourController.getTourStats);
-
 router
   .route('/top-5-cheap')
   .get(tourController.aliasTopTours, tourController.getAllTours);
-
 router
   .route('/')
   .get(tourController.getAllTours)
