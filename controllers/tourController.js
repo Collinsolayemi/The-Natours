3c67c6736912c8221d82a5c38f6160c930a3ad15
collinsolayemi <collinsolayemi@gmail.com>
2023-02-13 00:33:13 -0800

    updating the tour controllers with error handling

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -36,11 +36,9 @@ exports.getAllTours = catchAsync(async (req, res, next) => {
 //controller to get one certain tour
 exports.getOneTour = catchAsync(async (req, res, next) => {
   const tour = await Tour.findById(req.params.id);
-
   if (!tour) {
-    next(AppError('No tour found with that ID', 404));
+    return next(new AppError('No tour found with that ID', 404));
   }
-
   res.status(200).json({
     status: 'success',
     data: {
@@ -67,6 +65,9 @@ exports.updateTour = catchAsync(async (req, res, next) => {
     new: true,
     runValidators: true,
   });
+  if (!tour) {
+    return next(new AppError('No tour found with that ID', 404));
+  }
   res.status(200).json({
     status: 'success',
     data: {
@@ -77,7 +78,10 @@ exports.updateTour = catchAsync(async (req, res, next) => {
 
 //deleting a tour
 exports.deleteTour = catchAsync(async (req, res, next) => {
-  await Tour.findByIdAndDelete(req.params.id);
+  const tour = await Tour.findByIdAndDelete(req.params.id);
+  if (!tour) {
+    return next(new AppError('No tour found with that ID', 404));
+  }
   res.status(204).json({
     status: 'success',
     data: null,
