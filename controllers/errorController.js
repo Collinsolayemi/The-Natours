870bfd7b640d9b0a2b668d950d92546d853ec641
collinsolayemi <collinsolayemi@gmail.com>
2023-02-13 01:10:43 -0800

    implementing dev and prod erroe

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,10 +1,25 @@
+const sendErrorDev = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
+  });
+};
+
+const sendErrorProd = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
+  });
+};
+
 module.exports = (err, req, res, next) => {
   //   console.log(err.stack);
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
 
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
+  if (process.env.NODE_ENV === 'development') {
+    sendErrorDev(err, res);
+  } else if (process.env.NODE_ENV === 'development') {
+    sendErrorProd(err, res);
+  }
 };
