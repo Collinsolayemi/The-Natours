5fc96c37a0f055a04c8a33c9954281cfe1865f57
collinsolayemi <collinsolayemi@gmail.com>
2023-02-14 01:23:02 -0800

    creating a token for a user signup

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,12 +1,24 @@
-const Users = require('../model/userModel');
+const jwt = require('jsonwebtoken');
+const User = require('../model/userModel');
 const catchAsync = require('../utilities/catchAsync');
 
 //creating a sign up middleware functionality
 exports.signUp = catchAsync(async (req, res, next) => {
-  const newUser = await Users.create(req.body);
+  const newUser = await User.create({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+    confirmPassword: req.body.confirmPassword,
+  });
+
+  //creating a token for user when they signup
+  const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
 
   res.status(201).json({
     status: 'success',
+    token,
     data: {
       user: newUser,
     },
