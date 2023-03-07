51501dad6e3042a6b39acc8b49162f590dadb51a
collinsolayemi <collinsolayemi@gmail.com>
2023-03-07 04:05:50 -0800

    data sanitization against injection and xss

--- apps.js
+++ apps.js
@@ -2,9 +2,11 @@ const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+
 const AppError = require('./utilities/appError');
 const globalHandlerError = require('./controllers/errorController');
-
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
@@ -30,6 +32,12 @@ app.use('/api', limiter); //the limiter work on all routes starting with /api
 
 //Body parser, reading data from body into req.body
 app.use(express.json({ limit: '10kb' }));
+
+//Data sanitization against NOSQL query injection
+app.use(mongoSanitize());
+
+//Data sanitization against xss
+app.use(xss());
 
 //serving static files
 app.use(express.static(`${__dirname}/public`));
