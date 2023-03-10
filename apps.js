dd99e880c3808f02e4779883a3809d6cb21b131e
collinsolayemi <collinsolayemi@gmail.com>
2023-03-10 01:35:47 -0800

    preventing parameter pollution

--- apps.js
+++ apps.js
@@ -4,6 +4,7 @@ const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
 const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
+const hpp = require('hpp');
 
 const AppError = require('./utilities/appError');
 const globalHandlerError = require('./controllers/errorController');
@@ -38,6 +39,19 @@ app.use(mongoSanitize());
 
 //Data sanitization against xss
 app.use(xss());
+
+//protect against parameter pollution
+app.use(
+  hpp({
+    whitelist: [
+      'duration',
+      'ratingsAverage',
+      'ratingsQuantity',
+      'difficulty',
+      'price',
+    ],
+  })
+);
 
 //serving static files
 app.use(express.static(`${__dirname}/public`));
