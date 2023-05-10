8955cf8b4af790b11789771a85e0e088a8c4d393
Collinsolayemi <collinsolayemi@gmail.com>
2023-05-10 16:18:06 -0700

    code restructure

--- apps.js
+++ apps.js
@@ -10,6 +10,7 @@ const AppError = require('./utilities/appError');
 const globalHandlerError = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
+const reviewRouter = require('./routes/reviewRoutes');
 
 const app = express();
 //middleware
@@ -65,6 +66,7 @@ app.use((req, res, next) => {
 // middleware for the routes url
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+app.use('/api/v1/reviews', reviewRouter);
 
 //middleware advance error handler
 app.all('*', (req, res, next) => {
--- routes/reviewRoute.js
+++ routes/reviewRoute.js
@@ -1,1 +1,8 @@
+const express = require('express');
+
 const reviewController = require('../controllers/review-controller');
+const router = express.router();
+
+router.route('/').get(reviewController.getAllReviews)
+
+module.exports = router;
