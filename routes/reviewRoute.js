1901824689d980db1f53d7d54ac32bc0f2f59f4b
Collinsolayemi <collinsolayemi@gmail.com>
2023-06-26 17:00:24 -0700

    code restructure

--- routes/reviewRoute.js
+++ routes/reviewRoute.js
@@ -1,8 +1,15 @@
 const express = require('express');
-
+const authController = require('../controllers/authController');
 const reviewController = require('../controllers/review-controller');
 const router = express.Router();
 
-router.route('/').get(reviewController.getAllReviews);
+router
+  .route('/')
+  .get(reviewController.getAllReviews)
+  .post(
+    authController.protect,
+    authController.restrictTo('user'),
+    reviewController.createReview
+  );
 
 module.exports = router;
